7dc6914c1480b769c0d1148e9cf8f08e3cc112ce
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2020-08-14 14:43:01 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1 style="color:blue;text-align:center;">Create Table & Insert Data into Table</h1>
+
+<p style="color:green;text-align:center;"><a href="dbtable.php">Create Table</a></p>
+<p style="color:green;text-align:center;"><a href="datainsert.php">Insert Data</a></p>
+
+</body>
+</html>
